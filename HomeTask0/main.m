9f73766e63a996e6493b664e8f2ad8404afd9d88
Siarhei Kokhan <Siarhei_Kokhan@epam.com>
2018-03-26 16:41:57 +0300

    Initial Commit

---
+++ HomeTask0/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HomeTask0
+//
+//  Created by Siarhei Kokhan on 3/26/18.
+//  Copyright © 2018 Siarhei Kokhan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
